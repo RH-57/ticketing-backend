b8e18ffc4c06100c99e40e2446bbc09884742193
rahmat <rahmatbachtiar64@gmail.com>
2025-04-11 10:42:56 +0700

    create compare chart subcategory by type and ignore cors.js

--- middlewares/cors.js
+++
@@ -1,8 +0,0 @@
-const cors = require('cors');
-
-app.use(cors({
-    origin: 'http://192.168.1.185:5173', // Sesuaikan dengan URL frontend (React)
-    credentials: true, // Izinkan cookies dikirim dari frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Izinkan metode HTTP tertentu
-    allowedHeaders: ['Content-Type', 'Authorization', 'csrf-token'], // Izinkan header tertentu
-}));
